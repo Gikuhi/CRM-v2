237134d3400302542ccdb57fe8d980ceb5675d80
Gikuhi <richardgikm@gmail.com>
2025-10-14 07:30:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2242820018-694b2",
+  "appId": "1:74373409129:web:272a6589a5e109236735f8",
+  "apiKey": "AIzaSyCEkjJAHjgo22IJNSsbe1IQ4L2VCTk63p0",
+  "authDomain": "studio-2242820018-694b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "74373409129"
+};
