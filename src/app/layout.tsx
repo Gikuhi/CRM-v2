7b621833aef68b36ea7dbf51e832d838cef3199f
Gikuhi <richardgikm@gmail.com>
2025-10-27 09:36:16 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
-import { AppLayout } from '@/components/app-layout';
 
 export const metadata: Metadata = {
   title: 'CollectPro',
@@ -23,9 +22,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
-          <AppLayout>
             {children}
-          </AppLayout>
         </FirebaseClientProvider>
         <Toaster />
       </body>
