fc5adf5eb2652bd4951cfe38ef5695d0ccd89a6c
Gikuhi <richardgikm@gmail.com>
2025-12-09 09:23:26 +0000

    sure its fixed?

--- src/app/(app)/admin/user-management/page.tsx
+++ src/app/(app)/admin/user-management/page.tsx
@@ -211,16 +211,16 @@ export default function UserManagementMasterPage() {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Team</FormLabel>
-                     <Select onValueChange={field.onChange} defaultValue={field.value}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a team" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {mockTeams.map(team => <SelectItem key={team.team_id} value={team.team_name}>{team.team_name}</SelectItem>)}
-                        </SelectContent>
-                      </Select>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a team" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {mockTeams.map(team => <SelectItem key={team.team_id} value={team.team_name}>{team.team_name}</SelectItem>)}
+                      </SelectContent>
+                    </Select>
                     <FormMessage />
                   </FormItem>
                 )}
@@ -437,5 +437,7 @@ export default function UserManagementMasterPage() {
     </div>
   );
 }
+
+    
 
     
